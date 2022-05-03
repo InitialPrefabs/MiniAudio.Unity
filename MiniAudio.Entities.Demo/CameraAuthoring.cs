b2aed9399bb73f90545fbf16deb671e49ea4378d
Porrith Suong <porrith.suong@gmail.com>
2022-05-03 19:46:15 -0400

    Cleans up the camera authoring.

--- MiniAudio.Entities.Demo/CameraAuthoring.cs
+++ MiniAudio.Entities.Demo/CameraAuthoring.cs
@@ -1,29 +1,8 @@
-using InitialPrefabs.NimGui.Loop;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace MiniAudio.Entities.Demo {
 
     [System.Obsolete]
     public class CameraAuthoring : MonoBehaviour {
-
-        public CameraEvent CameraEvent;
-        public Camera Camera;
-
-        void OnEnable() {
-#if !URP_ENABLED
-            DefaultImGuiInitialization.SetupCamera(Camera, CameraEvent);
-#endif
-        }
-
-        void OnDisable() {
-#if !URP_ENABLED
-            DefaultImGuiInitialization.TearDownCamera(Camera, CameraEvent);
-#endif
-        }
-
-        void Update() {
-
-        }
     }
 }
