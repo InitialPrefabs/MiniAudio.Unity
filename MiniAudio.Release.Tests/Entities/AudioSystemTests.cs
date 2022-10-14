b6cab13fd7066a31d05451b88abfaadc7e4e1eb6
Porrith Suong <porrith.suong@gmail.com>
2022-10-13 21:48:27 -0400

    Adds runtime tests for the AudioSystem.

--- MiniAudio.Release.Tests/Entities/AudioSystemTests.cs
+++ MiniAudio.Release.Tests/Entities/AudioSystemTests.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections;
 using MiniAudio.Entities;
+using NUnit.Framework;
 using Unity.Entities;
+using Unity.Scenes;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using AudioClip = MiniAudio.Entities.AudioClip;
+using Object = UnityEngine.Object;
 
 namespace MiniAudio.Interop.Tests {
 
@@ -14,21 +19,106 @@ namespace MiniAudio.Interop.Tests {
 
             protected override void OnUpdate() { }
 
-            public void AudioSystemPlaysSound() {
-                foreach (var (path, entity) in SystemAPI.Query<Path>().WithEntityAccess()) {
+            void AssertAudioClip(Action<AudioClip> callback) {
+                foreach (var (audioClip, _) in SystemAPI
+                    .Query<AudioClip, Path>()) {
+
+                    Assert.AreNotEqual(audioClip.Handle, uint.MaxValue);
+                    callback?.Invoke(audioClip);
                 }
+            }
+
+            public void PlaysAudio() {
+                foreach (var (audioClip, _, entity) in SystemAPI
+                    .Query<AudioClip, Path>()
+                    .WithEntityAccess()) {
+
+                    Assert.AreNotEqual(audioClip.Handle, uint.MaxValue);
+
+                    var playAudioClip = audioClip;
+                    playAudioClip.CurrentState = AudioState.Playing;
+                    EntityManager.SetComponentData(entity, playAudioClip);
+                }
+            }
+
+            public void PauseAudio() {
+                foreach (var (audioClip, _, entity) in SystemAPI
+                    .Query<AudioClip, Path>().WithEntityAccess()) {
+
+                    Assert.AreNotEqual(audioClip.Handle, uint.MaxValue);
+                    var pausedAudioClip = audioClip;
+                    pausedAudioClip.CurrentState = AudioState.Paused;
+
+                    EntityManager.SetComponentData(entity, pausedAudioClip);
+                }
+            }
+
+            public void StopAudio() {
+                foreach (var (audioClip, _, entity) in SystemAPI
+                    .Query<AudioClip, Path>().WithEntityAccess()) {
+
+                    Assert.AreNotEqual(audioClip.Handle, uint.MaxValue);
+                    var pausedAudioClip = audioClip;
+                    pausedAudioClip.CurrentState = AudioState.Stopped;
+
+                    EntityManager.SetComponentData(entity, pausedAudioClip);
+                }
+            }
+
+            public void AudioIsPlaying() {
+                AssertAudioClip(clip => Assert.AreEqual(AudioState.Playing, clip.CurrentState));
+            }
+
+            public void AudioIsPaused() {
+                AssertAudioClip(clip => Assert.AreEqual(AudioState.Paused, clip.CurrentState));
+            }
+
+            public void AudioIsStopped() {
+                AssertAudioClip(clip => Assert.AreEqual(AudioState.Stopped, clip.CurrentState));
             }
         }
 
+        SceneSystemHelper sceneSystemHelper;
+        TestAudioSystem testAudioSystem;
 
         [UnitySetUp]
         public IEnumerator SetUp() {
             yield return SceneManager.LoadSceneAsync("Scenes/MainSampleScene");
+            sceneSystemHelper = World
+                .DefaultGameObjectInjectionWorld
+                .GetOrCreateSystemManaged<SceneSystemHelper>();
+
+            testAudioSystem = World
+                .DefaultGameObjectInjectionWorld
+                .GetOrCreateSystemManaged<TestAudioSystem>();
         }
 
         [UnityTest]
-        public IEnumerator AudioIsLoaded() {
-            throw new NotImplementedException();
+        public IEnumerator PersistentAudioLifeCycle() {
+            var subScene = Object.FindObjectOfType<SubScene>();
+            SceneSystem.LoadSceneAsync(World.DefaultGameObjectInjectionWorld.Unmanaged, subScene.SceneGUID);
+
+            while (!sceneSystemHelper.AreScenesLoaded(subScene)) {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.5f);
+            testAudioSystem.PlaysAudio();
+
+            yield return new WaitForSeconds(0.1f);
+            testAudioSystem.AudioIsPlaying();
+
+            yield return new WaitForSeconds(0.1f);
+            testAudioSystem.PauseAudio();
+
+            yield return new WaitForSeconds(0.1f);
+            testAudioSystem.AudioIsPaused();
+
+            yield return new WaitForSeconds(0.1f);
+            testAudioSystem.StopAudio();
+            
+            yield return new WaitForSeconds(0.1f);
+            testAudioSystem.AudioIsStopped();
         }
     }
 }
---
+++ MiniAudio.Release.Tests/Entities/SceneSystemHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Scenes;
+
+namespace MiniAudio.Interop.Tests {
+
+    [DisableAutoCreation]
+    public partial class SceneSystemHelper : SystemBase {
+
+        protected override void OnUpdate() { }
+
+        Entity[] GetSceneGuidEntities(params SubScene[] subScenes) {
+            var entities = new List<Entity>(subScenes.Length);
+            foreach (var subScene in subScenes) {
+                foreach (var (sceneReference, entity) in SystemAPI
+                    .Query<SceneReference>().WithEntityAccess()) {
+
+                    if (sceneReference.SceneGUID == subScene.SceneGUID) {
+                        entities.Add(entity);
+                    }
+                }
+            }
+            return entities.ToArray();
+        }
+
+        public bool AreScenesLoaded(params SubScene[] subScenes) {
+            var entities = GetSceneGuidEntities(subScenes);
+            var isLoaded = true;
+            for (int i = 0; i < entities.Length; i++) {
+                var guid = subScenes[i].SceneGUID;
+                isLoaded &= SceneSystem.IsSceneLoaded(World.Unmanaged, entities[i]);
+            }
+            return isLoaded;
+        }
+    }
+}
